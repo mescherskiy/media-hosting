605c3f4a83ea73f6669dc7cc48f64f696195ccf0
Alexander <mescherskiy.alexandr@gmail.com>
2023-10-04 21:57:09 +0200

    renderPhoto fixed

--- frontend/src/app/components/PhotoGallery.js
+++ frontend/src/app/components/PhotoGallery.js
@@ -74,7 +74,7 @@ const PhotoGallery = () => {
                             strokeWidth="1.5" 
                             stroke="currentColor" 
                             className={`w-6 h-6 select-icon ${isSelected ? "selected" : ""}`}
-                            onClick={handleToggleSelection(index)}>
+                            onClick={() => handleToggleSelection(index)}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </div>
