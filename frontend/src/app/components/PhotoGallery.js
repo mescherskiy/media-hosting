88c8c0892a3a45488099bcd0e2b964db1f94b0de
Alexander <mescherskiy.alexandr@gmail.com>
2023-10-05 21:38:28 +0200

    renderPhoto fixed

--- frontend/src/app/components/PhotoGallery.js
+++ frontend/src/app/components/PhotoGallery.js
@@ -61,15 +61,15 @@ const PhotoGallery = () => {
                 onClick={handlePhotoClick}
                 spacing={8}
                 renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto, layout }) => (
-                    <div style={{position: "relative", ...wrapperStyle}}>
+                    <div className="photo-item" style={{position: "relative", ...wrapperStyle}}>
                         {renderDefaultPhoto({wrapped: true})}
-                        <div className="photo-overlay">
+                        <div className={`photo-overlay ${photo.isSelected ? "selected" : ""}`}>
                             <svg 
                             xmlns="http://www.w3.org/2000/svg" 
                             fill="white" 
                             viewBox="0 0 24 24" 
-                            strokeWidth="1.5" 
-                            stroke="currentColor" 
+                            strokeWidth="1" 
+                            stroke="none" 
                             className={`w-6 h-6 select-icon ${photo.isSelected ? "selected" : ""}`}
                             onClick={() => handleToggleSelection(layout.index)}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
