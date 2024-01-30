7322d8727004562b61ad96c90663d30dd258c7fc
Alexander <mescherskiy.alexandr@gmail.com>
2024-01-30 21:40:31 +0100

    try to fix client-side routing

--- src/main/java/ua/com/mescherskiy/mediahosting/controllers/TestController.java
+++ src/main/java/ua/com/mescherskiy/mediahosting/controllers/TestController.java
@@ -3,12 +3,13 @@ package ua.com.mescherskiy.mediahosting.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ua.com.mescherskiy.mediahosting.payload.response.MessageResponse;
 
 //@CrossOrigin(origins = "*", maxAge = 3600)
 //@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true", maxAge = 3600)
-@RestController
+@Controller
 public class TestController {
 
     @GetMapping()
